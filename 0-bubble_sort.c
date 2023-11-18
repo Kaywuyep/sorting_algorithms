344374d4d63068fdb236727eb169a23ed08f8aa9
Kaywuyep <wuyepkate@gmail.com>
2023-11-18 14:45:05 -0800

    task

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -12,9 +12,9 @@ void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
 
-	for(i = 0; i < size; i++)
+	for (i = 0; i < size; i++)
 	{
-		for(j = 0; j < size; j++)
+		for (j = 0; j < size; j++)
 		{
 			if (array[j] > array[j + 1] && array[j + 1])
 			{
