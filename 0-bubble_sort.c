a1397a52a280af58066e138db414dae9395195ae
Kaywuyep <wuyepkate@gmail.com>
2023-11-19 03:40:37 -0800

    task 0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,5 +1,19 @@
 #include "sort.h"
 /**
+ * swap - a function that swaps two variables
+ * @a: variable
+ * @b: variable
+ */
+void swap(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+/**
  * bubble_sort - a function that compares two elmts and swaps
  * @array: array of elmets to be sorted
  * @size:m size of the array
@@ -12,9 +26,10 @@ void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
 
+	if (size < 2)
+		return;
 	for (i = 0; i < size; i++)
-	{
-		for (j = 0; j < size; j++)
+		for (j = 0; j < size; j++)/*where j is the index*/
 		{
 			if (array[j] > array[j + 1] && array[j + 1])
 			{
@@ -22,6 +37,4 @@ void bubble_sort(int *array, size_t size)
 				print_array(array, size);
 			}
 		}
-	}
-	print_array(array, size);
 }
