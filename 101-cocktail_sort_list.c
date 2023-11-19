0ee362f6a90de0ac8e942d6f0bc25393a8bfd34d
Kaywuyep <wuyepkate@gmail.com>
2023-11-19 05:33:47 -0800

    task

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -1,8 +1,42 @@
 #include "sort.h"
-
 /**
- * cocktail_sort_list - Sorts a doubly linked list of integers in ascending order
- *                      using the Cocktail shaker sort algorithm.
+ * swap_nodes - Swaps a node with its previous node in a doubly linked list
+ * @list: Pointer to the head of the doubly linked list
+ * @node: Pointer to the node to be swapped with its previous node
+ */
+void swap_nodes(listint_t **list, listint_t *node)
+{
+	listint_t *prev, *prev_prev, *next;
+
+	if (node == NULL || node->prev == NULL)
+		return;
+
+	prev = node->prev;
+	prev_prev = prev->prev;
+	next = node->next;
+
+	if (prev_prev != NULL)
+	{
+		prev_prev->next = node;
+	}
+	else
+	{
+		*list = node;
+	}
+
+	node->prev = prev_prev;
+	node->next = prev;
+	prev->prev = node;
+	prev->next = next;
+
+	if (next != NULL)
+	{
+		next->prev = prev;
+	}
+}
+/**
+ * cocktail_sort_list - Sorts a doubly linked list of integers in
+ * ascending order using the Cocktail shaker sort algorithm.
  * @list: Pointer to the head of the doubly linked list
  */
 void cocktail_sort_list(listint_t **list)
