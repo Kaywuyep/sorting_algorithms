86f741e02e964653f0284ebbc7cb057cd5394c40
Kajad002 <preciouskay4real@gmail.com>
2023-11-20 14:16:20 -0800

    107-quick_sort_hoare.c

---
+++ 107-quick_sort_hoare.c
@@ -0,0 +1,77 @@
+#include "sort.h"
+
+/**
+ * hoare_partition - Order a subset of an array of integers
+ *                   according to the hoare partition scheme.
+ * @array: The array of integers.
+ * @size: The size of the array.
+ * @left: The starting index of the subset to order.
+ * @right: The ending index of the subset to order.
+ *
+ * Return: The final partition index.
+ *
+ * Description: Uses the last element of the partition as the pivot.
+ * Prints the array after each swap of two elements.
+ */
+int hoare_partition(int *array, size_t size, int left, int right)
+{
+	int pivot, top, buttom;
+
+	pivot = array[right];
+
+	for (top = left - 1, buttom = right + 1; top < buttom;)
+	{
+		do {
+			top++;
+		} while (array[top] < pivot);
+		do {
+			buttom--;
+		} while (array[buttom] > pivot);
+
+		if (top < buttom)
+		{
+			swap(array + top, array + buttom);
+			print_array(array, size);
+		}
+	}
+
+	return (top);
+}
+
+/**
+ * hoare_sort - Implement the quicksort algorithm through recursion.
+ * @array: An array of integers to sort.
+ * @size: The size of the array.
+ * @left: The starting index of the array partition to order.
+ * @right: The ending index of the array partition to order.
+ *
+ * Description: Uses the Hoare partition scheme.
+ */
+void hoare_sort(int *array, size_t size, int left, int right)
+{
+	int part;
+
+	if (right - left > 0)
+	{
+		part = hoare_partition(array, size, left, right);
+		hoare_sort(array, size, left, part - 1);
+		hoare_sort(array, size, part, right);
+	}
+}
+
+/**
+ * quick_sort_hoare - Sort an array of integers in ascending
+ *                    order using the quicksort algorithm.
+ * @array: An array of integers.
+ * @size: The size of the array.
+ *
+ * Description: Uses the Hoare partition scheme. Prints
+ * the array after each swap of two elements.
+ */
+void quick_sort_hoare(int *array, size_t size)
+{
+	if (array == NULL || size < 2)
+		return;
+
+	hoare_sort(array, size, 0, size - 1);
+}
